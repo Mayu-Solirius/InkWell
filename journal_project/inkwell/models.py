c3d1686e49f6e085b3414747934c73364a6609b2
Mayu <mayuran.sundar@solirius.com>
2023-07-26 16:16:20 +0100

    created journal entry model, added url and views for it

--- journal_project/inkwell/models.py
+++ journal_project/inkwell/models.py
@@ -1,3 +1,12 @@
+# journal_app/models.py
 from django.db import models
 
-# Create your models here.
+
+class JournalEntry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ journal_project/inkwell/urls.py
@@ -0,0 +1,7 @@
+# journal_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
