f795cddf6b447f44137fa7748abb3eff12a80d4d
Mayu <mayuran.sundar@solirius.com>
2023-07-26 16:57:23 +0100

    feature/2-added delete functionality for journal entries

--- journal_project/inkwell/views.py
+++ journal_project/inkwell/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import JournalEntry
 
 
 def index(request):
     entries = JournalEntry.objects.all().order_by("-updated_time")
     return render(request, "inkwell/index.html", {"entries": entries})
+
+
+def delete_journal_entry(request, pk):
+    entry = get_object_or_404(JournalEntry, pk=pk)
+    entry.delete()
+    return redirect("index")
