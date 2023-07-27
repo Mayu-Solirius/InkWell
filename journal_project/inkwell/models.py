4489c449ead20d8877a0df646b85f7d022f6ff4d
Mayu <mayuran.sundar@solirius.com>
2023-07-27 12:45:49 +0100

    Fix/7-Entries now can have empty title or content

--- journal_project/inkwell/models.py
+++ journal_project/inkwell/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class JournalEntry(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200, blank=True)
+    content = models.TextField(blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
--- journal_project/inkwell/views.py
+++ journal_project/inkwell/views.py
@@ -18,9 +18,31 @@ def delete_journal_entry(request, pk):
 def save_journal_entry(request):
     if request.method == "POST":
         entry_id = request.POST.get("entry_id")
+        title = request.POST.get("title").strip()
+        content = request.POST.get("content").strip()
+
+        if not title and not content:
+            # If both title and content are empty, remove the entry from the database
+            if entry_id != "0":
+                entry = get_object_or_404(JournalEntry, pk=entry_id)
+                entry.delete()
+            return JsonResponse({"status": "success"})
+
         if entry_id == "0":
-            # Create a new entry
-            form = JournalEntryForm(request.POST)
+            # Create a new entry only if either title or content is not empty
+            if title or content:
+                form = JournalEntryForm(request.POST)
+            else:
+                return JsonResponse(
+                    {
+                        "status": "error",
+                        "errors": {
+                            "non_field_errors": [
+                                "Both title and content cannot be empty."
+                            ]
+                        },
+                    }
+                )
         else:
             # Update an existing entry
             entry = JournalEntry.objects.get(pk=entry_id)
