8cdadf4e8f00cd00140453f2c80c0efa35fbcab9
Mayu <mayuran.sundar@solirius.com>
2023-07-28 18:14:36 +0100

    Feature/8-Created a register app for  account signup

--- journal_project/journal_project/settings.py
+++ journal_project/journal_project/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "inkwell.apps.InkwellConfig",
+    "register.apps.RegisterConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- journal_project/journal_project/urls.py
+++ journal_project/journal_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from register import views as rv
 
 urlpatterns = [
     path("", include("inkwell.urls")),
+    path("register/", rv.register, name="register"),
     path("admin/", admin.site.urls),
 ]
