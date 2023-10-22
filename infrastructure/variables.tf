f5291fb987b2eb907a64b436b4bea65286029993
Mayu <mayuran.sundar@solirius.com>
2023-10-22 17:19:33 +0100

    Updated pipelines

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -23,29 +23,14 @@ variable "app_plan_name" {
   description = "App Service Plan name"
 }
 
-# Extra variables
-variable "container_registry_url" {
-  type        = string
-  description = "URL of the container registry"
-}
-
-variable "repository_name" {
-  type        = string
-  description = "Name of the repository"
-}
-
 variable "image_name" {
   type        = string
   description = "Name of the Docker image"
-}
-
-variable "image_tag" {
-  type        = string
-  description = "Tag or version of the Docker image"
+  default     = "inkwell-img"
 }
 
 variable "docker_image_url" {
   type        = string
-  description = "Docker image URL in the format '<container-registry-url>/<repository-name>/<image-name>:<tag>'"
-  default     = "${var.container_registry_url}/${var.repository_name}/${var.image_name}:${var.image_tag}"
+  description = "Docker image URL in the format"
+  default     = "${var.acr_name}.azurecr.io/${var.image_name}:latest"
 }
