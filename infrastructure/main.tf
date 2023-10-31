831215baa7c19bade0257501440aea3fe7551bce
Mayu <mayuran.sundar@solirius.com>
2023-10-31 19:51:18 +0000

    Added acr_pull access to app service

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,12 +33,12 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 }
 
-# resource "azurerm_role_assignment" "role_acrpull" {
-#   scope                            = azurerm_container_registry.acr.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_linux_web_app.webapp.identity[0].principal_id
+  skip_service_principal_aad_check = true
+}
 
 # resource "azurerm_kubernetes_cluster" "aks" {
 #   name                = var.cluster_name
@@ -61,4 +61,11 @@ resource "azurerm_linux_web_app" "webapp" {
 #   tags = {
 #     nodepooltype = "free"
 #   }
+# }
+
+# resource "azurerm_role_assignment" "role_acrpull" {
+#   scope                            = azurerm_container_registry.acr.id
+#   role_definition_name             = "AcrPull"
+#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+#   skip_service_principal_aad_check = true
 # }
